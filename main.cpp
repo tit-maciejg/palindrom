6973372a5100de6218dcc9799098aadccf574f9f
tit-maciejg <64011482+tit-maciejg@users.noreply.github.com>
2020-05-11 15:28:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+string s;
+int a=0;
+int suma=0;
+bool palindrom (string s)
+{
+    int dl=s.size();
+    for (int i=0;i<dl/2;i++)
+        if (s[i]!=s[dl-i-1]) return false;
+    return true;
+}
+fstream dane;
+int main()
+{
+    cout << "Oto twoje palindromy:" << endl;
+    dane.open("dane.txt",ios::in);
+     while (dane.good())
+    {
+        dane>>s;
+        if (palindrom(s)) cout << s << endl;
+        for (int i=0;i<1000;i++);
+        {
+           if (palindrom(s)) suma=suma+1;
+        }
+    }
+    dane.close();
+    cout <<"Oto suma twoich palindromow: " << suma << endl;
+    cout << endl;
+    cout << "Program Macieja Grzechowiaka" << endl;
+    return 0;
+}
